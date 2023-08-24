329483c4f583d0a833001b5ba5cc22ca9a8c6748
PouyaGohari <pouya.hmgohari@gmail.com>
2023-08-24 16:41:52 +0330

    Define a Inputshape struct

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include <fstream>
+using namespace std;
+struct Input_shape{
+    int time_slot_counts;
+    vector<string> participants;
+    vector<vector<string>> votes;
+};
 
 int main(){
     return 0;
