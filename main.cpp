8b9f808fb32b3469fbffa0c8a4c741871a3bed78
PouyaGohari <pouya.hmgohari@gmail.com>
2023-08-26 11:22:23 +0330

    Filling input_shape with lines of captured by reading from a file function

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <sstream>
 using namespace std;
 
+const char pipe = '|';
+const char colon = ':';
+const char space = ' ';
 struct Input_shape{
     int time_slot_counts;
     vector<string> participants;
@@ -20,6 +23,43 @@ vector<string> reading_from_file(string file_name){
     return result;
 }
 
+void replace_symbols_with_space(string &line){
+    for(auto &character : line){
+        if(character == pipe || character == colon){
+            character = space;
+        }
+    }
+}
+
+vector<string> break_line_to_vector(string line){
+    istringstream iss(line);
+    vector<string> names;
+    string name;
+    while (iss >> name){
+        names.push_back(name);
+    }
+    return names;
+}
+
+vector<vector<string>> filling_votes(vector<string> lines){
+    vector<vector<string>> votes;
+    for(auto line : lines){
+        replace_symbols_with_space(line);
+        votes.push_back(break_line_to_vector(line));
+    }
+    return votes;
+}
+
+Input_shape filing_input_shape(vector<string> lines){
+    vector<string> new_lines(lines.begin() + 2, lines.end());
+    Input_shape reordered;
+    reordered.time_slot_counts = stoi(lines[0]);
+    reordered.participants = break_line_to_vector(lines[1]);
+    reordered.votes = filling_votes(new_lines);
+    return reordered;
+}
+
 int main(int argc, char *argv[]){
+    Input_shape inputs = filing_input_shape(reading_from_file(argv[1]));
     return 0;
 }
