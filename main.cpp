490c7f79175292faed3a477997387910496f5438
PouyaGohari <pouya.hmgohari@gmail.com>
2023-08-26 14:07:41 +0330

    Remove 2 line of last function in order to code have better readability

--- main.cpp
+++ main.cpp
@@ -69,20 +69,18 @@ Input_shape filing_input_shape(vector<string> lines){
     return reordered;
 }
 
-vector<time_slot> initialize_time_slots(int time_slot_counts, int number_of_participants){
+vector<time_slot> initialize_time_slots(int time_slot_counts){
     vector<time_slot> whole_time_slots(time_slot_counts);
-    vector<string> participants(number_of_participants);
     int i = 0;
     for(auto &slot : whole_time_slots){
         slot.id = i;
         slot.if_numbers = time_slot_counts;
         slot.no_numbers = 0;
         slot.yes_numbers = 0;
-        slot.no_participants = slot.if_participants = slot.yes_participants = participants;
         i += 1;
     }
     return whole_time_slots;
-};
+}
 
 int main(int argc, char *argv[]){
     Input_shape inputs = filing_input_shape(reading_from_file(argv[1]));
