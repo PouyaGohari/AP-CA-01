4bab7500a29c915daeb22793600e05c2bf98d459
PouyaGohari <pouya.hmgohari@gmail.com>
2023-08-26 10:52:02 +0330

    Change the a getttin_input function name to reading_from_file

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <sstream>
 using namespace std;
+
 struct Input_shape{
     int time_slot_counts;
     vector<string> participants;
     vector<vector<string>> votes;
 };
 
-vector<string> getting_input(string file_name){
+vector<string> reading_from_file(string file_name){
     string line;
     vector<string> result;
     ifstream input_file(file_name);
