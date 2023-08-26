7421bda193e3a35a02ae62fede51a57366fc1dfb
PouyaGohari <pouya.hmgohari@gmail.com>
2023-08-26 13:49:08 +0330

    Defined another struct called time_slot and then initialized them

--- main.cpp
+++ main.cpp
@@ -13,6 +13,16 @@ struct Input_shape{
     vector<vector<string>> votes;
 };
 
+struct time_slot{
+    int id;
+    int yes_numbers;
+    int no_numbers;
+    int if_numbers;
+    vector<string> yes_participants;
+    vector<string> no_participants;
+    vector<string> if_participants;
+};
+
 vector<string> reading_from_file(string file_name){
     string line;
     vector<string> result;
@@ -58,6 +68,21 @@ Input_shape filing_input_shape(vector<string> lines){
     reordered.votes = filling_votes(new_lines);
     return reordered;
 }
+
+vector<time_slot> initialize_time_slots(int time_slot_counts, int number_of_participants){
+    vector<time_slot> whole_time_slots(time_slot_counts);
+    vector<string> participants(number_of_participants);
+    int i = 0;
+    for(auto &slot : whole_time_slots){
+        slot.id = i;
+        slot.if_numbers = time_slot_counts;
+        slot.no_numbers = 0;
+        slot.yes_numbers = 0;
+        slot.no_participants = slot.if_participants = slot.yes_participants = participants;
+        i += 1;
+    }
+    return whole_time_slots;
+};
 
 int main(int argc, char *argv[]){
     Input_shape inputs = filing_input_shape(reading_from_file(argv[1]));
